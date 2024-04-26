e230c47f60840671ce7c2119e7f6f5b8e7819525
Mazed1413 <mamazed1314@gmail.com>
2024-04-26 18:59:39 +0600

    deploy in vercel

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.User_DB}:${process.env.UserPass_DB}@cluster0.tvtcgta.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -23,7 +29,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const craftCollection = client.db("artisticAuraDB").collection("craft");
 
@@ -33,7 +39,15 @@ async function run() {
       res.send(result);
     });
 
-    app.post("/craft", async (req, res) => {
+    app.get("/myCraft/:email", async (req, res) => {
+      console.log(req.params.email);
+      const result = await craftCollection
+        .find({ email: req.params.email })
+        .toArray();
+      res.send(result);
+    });
+
+    app.post("/addCraft", async (req, res) => {
       const addNewItem = req.body;
       console.log(addNewItem);
       const result = await craftCollection.insertOne(addNewItem);
@@ -52,6 +66,9 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
 app.listen(port, () => {
   console.log(`Artistic Aura Server is running on port: ${port}`);
 });
